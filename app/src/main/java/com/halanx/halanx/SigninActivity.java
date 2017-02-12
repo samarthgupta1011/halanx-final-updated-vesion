26ddd1c70161767b738b2747b470dfa9d1350d04
Samarth Gupta <samarthgupta1011@gmail.com>
2017-02-12 22:41:49 +0530

    Sam commit

---
+++ app/src/main/java/com/halanx/halanx/SigninActivity.java
@@ -0,0 +1,22 @@
+package com.halanx.halanx;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by samarthgupta on 12/02/17.
+ */
+
+//To delete - signin. java and register xml old layout
+
+public class SigninActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+
+    }
+}
